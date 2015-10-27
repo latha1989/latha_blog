291f1d371d35c36ee1b3485fcde85417a76f0851
nav0285 <nav0285@gmail.com>
2015-10-27 18:47:38 -0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LathaBlog::Application.config.secret_key_base = '7b686774993d200ac9e369a390b9566dd7f326eb1b69bb67140d37170decd44654b6d38a9de75bd558a922bf62270407a86a6a47ef6125763ae17d821a239cf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LathaBlog::Application.config.session_store :cookie_store, key: '_latha_blog_session'
